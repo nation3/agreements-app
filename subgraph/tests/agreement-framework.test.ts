2c060ebb5c79b9e2729f5ecb28749f979a385488
Mitesh Mutha <mtshmtha@gmail.com>
2022-08-05 22:19:07 +0200

    Added tests for AgreementFinalizationSent event

--- subgraph/tests/agreement-framework.test.ts
+++ subgraph/tests/agreement-framework.test.ts
@@ -9,8 +9,14 @@ import { logStore } from 'matchstick-as/assembly/store'
 import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts"
 import { Agreement } from "../generated/schema"
 import { AgreementCreated } from "../generated/AgreementFramework/AgreementFramework"
-import { handleAgreementCreated, handleAgreementJoined } from "../src/agreement-framework"
-import { createAgreementCreatedEvent, createAgreementJoinedEvent, assertAgreement, assertAgreementPosition } from "./agreement-framework-utils"
+import { handleAgreementCreated, handleAgreementJoined, handleAgreementFinalizationSent } from "../src/agreement-framework"
+import { 
+  createAgreementCreatedEvent, 
+  createAgreementJoinedEvent, 
+  createAgreementFinalizationSentEvent,
+  assertAgreement, 
+  assertAgreementPosition
+} from "./agreement-framework-utils"
 
 describe("handling of AgreementCreated", () => {
   afterEach(() => {
@@ -126,5 +132,52 @@ describe("handling of AgreementJoined", () => {
     assertAgreementPosition("200-1", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e8", "1050", "Idle", "200")
     assertAgreementPosition("201-0", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e9", "2000", "Idle", "201")
     assertAgreementPosition("201-1", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43ea", "2050", "Idle", "201")
+  })
+})
+
+describe("handling of AgreementFinalizationSent", () => {
+  afterEach(() => {
+    clearStore()
+  })
+
+  test("1 Agreement 1 AgreementPosition Finalization", () => {
+    let newAgreementCreatedEvent = createAgreementCreatedEvent(BigInt.fromI32(200), Bytes.fromI32(1234567890), BigInt.fromI32(1000))
+    handleAgreementCreated(newAgreementCreatedEvent)
+
+    let newAgreementJoinedEvent = createAgreementJoinedEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7"), BigInt.fromI32(1000))
+    handleAgreementJoined(newAgreementJoinedEvent)
+
+    let newAgreementFinalizationSentEvent = createAgreementFinalizationSentEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7"))
+    handleAgreementFinalizationSent(newAgreementFinalizationSentEvent)
+
+    assert.entityCount("Agreement", 1)
+    assert.entityCount("AgreementPosition", 1)
+
+    assertAgreement("200", "0xd2029649", "1000", "[200-0]", "Created")
+    assertAgreementPosition("200-0", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7", "1000", "Finalized", "200")
+  })
+
+  test("1 Agreement 2 AgreementPosition Finalization", () => {
+    let newAgreementCreatedEvent = createAgreementCreatedEvent(BigInt.fromI32(200), Bytes.fromI32(1234567890), BigInt.fromI32(1000))
+    handleAgreementCreated(newAgreementCreatedEvent)
+
+    let newAgreementJoinedEvent = createAgreementJoinedEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7"), BigInt.fromI32(1000))
+    handleAgreementJoined(newAgreementJoinedEvent)
+
+    newAgreementJoinedEvent = createAgreementJoinedEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e8"), BigInt.fromI32(1050))
+    handleAgreementJoined(newAgreementJoinedEvent)
+
+    let newAgreementFinalizationSentEvent = createAgreementFinalizationSentEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7"))
+    handleAgreementFinalizationSent(newAgreementFinalizationSentEvent)
+
+    newAgreementFinalizationSentEvent = createAgreementFinalizationSentEvent(BigInt.fromI32(200), Address.fromString("0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e8"))
+    handleAgreementFinalizationSent(newAgreementFinalizationSentEvent)
+
+    assert.entityCount("Agreement", 1)
+    assert.entityCount("AgreementPosition", 2)
+
+    assertAgreement("200", "0xd2029649", "1000", "[200-0, 200-1]", "Created")
+    assertAgreementPosition("200-0", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7", "1000", "Finalized", "200")
+    assertAgreementPosition("200-1", "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e8", "1050", "Finalized", "200")
   })
 })
