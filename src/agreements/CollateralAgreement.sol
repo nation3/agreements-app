a279928e9ed78e58e3fc8a2fd8fbb1ec609f8b9e
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-17 10:33:44 +0800

    chore: upgrade to solidity 0.8.14
    close #33

--- src/agreements/CollateralAgreement.sol
+++ src/agreements/CollateralAgreement.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.14;
 
 import { ERC20 } from "solmate/src/tokens/ERC20.sol";
 import { SafeTransferLib } from "solmate/src/utils/SafeTransferLib.sol";
--- src/lib/Permit.sol
+++ src/lib/Permit.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.14;
 
 /// @dev EIP-2612 Permit
 ///      Together with a EIP-2612 compliant token,
