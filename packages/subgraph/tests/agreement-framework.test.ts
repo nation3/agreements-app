76d1a0a315849256221e2b2749f2febff53aadba
Mitesh Mutha <mtshmtha@gmail.com>
2022-08-30 22:02:45 +0200

    Cleaned test.

--- packages/subgraph/tests/agreement-framework.test.ts
+++ packages/subgraph/tests/agreement-framework.test.ts
@@ -27,59 +27,37 @@ import {
   assertAgreementPosition,
 } from "./agreement-framework-utils";
 
+const AGREEMENT_CREATED_EVENT_SAMPLE_1 = createAgreementCreatedEvent(
+  Bytes.fromI32(200),
+  Bytes.fromI32(1234567890),
+  BigInt.fromI32(1000),
+  "metadata"
+);
+
+const AGREEMENT_CREATED_EVENT_SAMPLE_2 = createAgreementCreatedEvent(
+  Bytes.fromI32(201),
+  Bytes.fromI32(1234567890),
+  BigInt.fromI32(1100),
+  "metadata"
+);
+
 describe("handling of AgreementCreated", () => {
   afterEach(() => {
     clearStore();
   });
 
-  beforeEach(() => {
-    let newAgreementCreatedEvent = createAgreementCreatedEvent(
-      Bytes.fromI32(200),
-      Bytes.fromI32(1234567890),
-      BigInt.fromI32(1000),
-      "metadata"
-    );
-    handleAgreementCreated(newAgreementCreatedEvent);
-  });
-
   test("1 AgreementCreated event", () => {
+    handleAgreementCreated(AGREEMENT_CREATED_EVENT_SAMPLE_1);
     assert.entityCount("Agreement", 1);
-    assertAgreement(
-      "0xc8000000",
-      "0xd2029649",
-      "1000",
-      "[]",
-      "Created",
-      "metadata"
-    );
+    assertAgreement("0xc8000000","0xd2029649","1000","[]","Created","metadata");
   });
 
   test("2 AgreementCreated event", () => {
-    let newAgreementCreatedEvent = createAgreementCreatedEvent(
-      Bytes.fromI32(201),
-      Bytes.fromI32(1234567890),
-      BigInt.fromI32(1100),
-      "metadata"
-    );
-    handleAgreementCreated(newAgreementCreatedEvent);
+    handleAgreementCreated(AGREEMENT_CREATED_EVENT_SAMPLE_1);
+    handleAgreementCreated(AGREEMENT_CREATED_EVENT_SAMPLE_2);
 
     assert.entityCount("Agreement", 2);
-
-    assertAgreement(
-      "0xc8000000",
-      "0xd2029649",
-      "1000",
-      "[]",
-      "Created",
-      "metadata"
-    );
-    assertAgreement(
-      "0xc9000000",
-      "0xd2029649",
-      "1100",
-      "[]",
-      "Created",
-      "metadata"
-    );
+    assertAgreement("0xc8000000","0xd2029649","1000","[]","Created","metadata");
+    assertAgreement("0xc9000000","0xd2029649","1100","[]","Created","metadata");
   });
 });
