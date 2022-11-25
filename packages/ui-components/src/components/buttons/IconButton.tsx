8b795bf0e432577e79527a37456e90c675e01f82
pythonpete32 <aaron@blackscale.io>
2022-11-25 11:32:34 +0400

    feat: do not pass  prop if disabled

--- packages/ui-components/src/components/buttons/IconButton.tsx
+++ packages/ui-components/src/components/buttons/IconButton.tsx
@@ -13,6 +13,7 @@ export const IconButton = ({
 	textColor = "white",
 	bgColor = "bluesky",
 	disabled,
+	onClick,
 	...props
 }: IconButtonProps) => {
 	return (
@@ -21,6 +22,7 @@ export const IconButton = ({
 				className={`text-${textColor} bg-${bgColor}-${disabled ? "300" : "400"} ${
 					!disabled && `hover:bg-${bgColor}-500`
 				} px-2 py-2`}
+				onClick={!disabled ? onClick : undefined}
 				{...props}
 			>
 				{icon}
