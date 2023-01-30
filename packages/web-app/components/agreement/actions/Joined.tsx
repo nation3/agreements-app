7d786abb94f384ce7b7875e99ed96988fb3230c6
xPi2 <uxiopineiro@gmail.com>
2023-01-30 14:10:40 +0100

    feat: Add dispute & finalize context modals

--- packages/web-app/components/agreement/actions/Joined.tsx
+++ packages/web-app/components/agreement/actions/Joined.tsx
@@ -1,8 +1,36 @@
 import { Button } from "@nation3/ui-components";
-import { useEffect } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import { useAgreementDispute, useAgreementFinalize } from "../../../hooks";
+import { Modal, ModalProps } from "flowbite-react";
+import { useTranslation } from "react-i18next";
+
+interface ContextModalProps extends Pick<ModalProps, "show" | "onClose"> {
+	title: string;
+	context: string;
+	button: ReactNode;
+}
+
+const ContextModal = ({ title, context, show, button, onClose }: ContextModalProps) => {
+	return (
+		<Modal show={show} onClose={onClose}>
+			<Modal.Header>
+				<div className="flex items-center w-full pl-3">
+					<h3 className="text-slate-600 text-xl md:text-xl font-semibold">{title}</h3>
+				</div>
+			</Modal.Header>
+			<div className="flex flex-col items-center justify-center gap-5 p-8 border-t-2 border-bluesky-200">
+				<div className="text-slate-600">{context}</div>
+				{button}
+			</div>
+		</Modal>
+	);
+};
 
 export const JoinedAgreementActions = ({ id }: { id: string }) => {
+	const { t } = useTranslation("common");
+	const [showDisputeModal, setDisputeModalVisibility] = useState(false);
+	const [showFinalizeModal, setFinalizeModalVisibility] = useState(false);
+
 	const {
 		dispute,
 		isLoading: disputeLoading,
@@ -22,22 +50,48 @@ export const JoinedAgreementActions = ({ id }: { id: string }) => {
 		}
 	}, [isDisputeSuccess, isFinalizeSuccess]);
 
+	const disputeButtonLoading = useMemo(
+		() => disputeLoading || disputeProcessing,
+		[disputeLoading, disputeProcessing],
+	);
+
+	const finalizeButtonLoading = useMemo(
+		() => finalizationLoading || finalizationProcessing,
+		[finalizationLoading, finalizationProcessing],
+	);
+
 	return (
-		<div className="flex gap-2 items-center justify-between">
-			<Button
-				label="Dispute"
-				bgColor="red"
-				isLoading={disputeLoading || disputeProcessing}
-				disabled={disputeLoading || disputeProcessing}
-				onClick={() => dispute()}
+		<>
+			<div className="flex gap-2 items-center justify-between">
+				<Button label="Dispute" onClick={() => setDisputeModalVisibility(true)} />
+				<Button label="Finalize" onClick={() => setFinalizeModalVisibility(true)} />
+			</div>
+			<ContextModal
+				title={t("dispute.title")}
+				context={t("dispute.description")}
+				show={showDisputeModal}
+				button={
+					<Button
+						label="Confirm Dispute"
+						isLoading={disputeButtonLoading}
+						onClick={() => dispute()}
+					/>
+				}
+				onClose={() => setDisputeModalVisibility(false)}
 			/>
-			<Button
-				label="Finalize"
-				bgColor="greensea"
-				isLoading={finalizationLoading || finalizationProcessing}
-				disabled={finalizationLoading || finalizationProcessing}
-				onClick={() => finalize()}
+			<ContextModal
+				title={t("finalize.title")}
+				context={t("finalize.description")}
+				show={showFinalizeModal}
+				button={
+					<Button
+						label="Confirm Finalization"
+						isLoading={finalizeButtonLoading}
+						onClick={() => finalize()}
+					/>
+				}
+				onClose={() => setFinalizeModalVisibility(false)}
 			/>
-		</div>
+		</>
 	);
 };
