b6ad59bbbce4d6eb469fc3f28c5d1882896df3ac
Gordo <gordo@gordo.design>
2023-02-08 22:11:50 +0000

    fix: mainnet constants

--- packages/web-app/lib/constants.tsx
+++ packages/web-app/lib/constants.tsx
@@ -2,7 +2,7 @@ import { BigNumber } from "ethers";
 
 export const permit2Address = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
 export const NATION = "0x333A4823466879eeF910A04D473505da62142069";
-export const frameworkAddress = "0x710617547D96573520b5f38668168DaFDBD9EA83";
+export const frameworkAddress = "0x2e41383506a6715da7dd0985dc401fe720e473c0";
 export const arbitratorAddress = "0x1dBEFF62DFa113f7254AaB5772F1AC6E66F94e7e";
 export const cohortAddress = "0xee8F0B89983aF83598E6Fb503B9f6e5eaf23b243";
 export const safeTxServiceUrl = "https://safe-transaction.mainnet.safe.global";
