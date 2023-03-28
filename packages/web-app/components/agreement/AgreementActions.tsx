02f8c64642f26d3db8493420f28c4d7dd5d17a9f
xPi2 <uxiopineiro@gmail.com>
2023-03-28 20:28:23 +0200

    fix: Migrate withdraw action to new action layout

--- packages/web-app/components/agreement/AgreementActions.tsx
+++ packages/web-app/components/agreement/AgreementActions.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { useAgreementData } from "./context/AgreementDataContext";
-import { AgreementFinalizedAlert, TermsVerificationAlert } from "../alerts";
 import { JoinableAgreementActions } from "./actions/Joinable";
 import { JoinedAgreementActions } from "./actions/Joined";
 import { FinalizedAgreementActions } from "./actions/Finalized";
@@ -8,24 +7,15 @@ import { FinalizedAgreementActions } from "./actions/Finalized";
 export const AgreementActions = (): ReactElement => {
 	const { id, userPosition, status: agreementStatus } = useAgreementData();
 
-	if (["Created", "Ongoing"].includes(String(agreementStatus))) {
-		if (userPosition && userPosition.status == 0)
-			return (
-				<>
-					{/* TODO: Not needed here */}
-					{/* <TermsVerificationAlert /> */}
-					<JoinableAgreementActions id={id} userPosition={userPosition} />
-				</>
-			);
-		if (userPosition && (userPosition.status == 1 || userPosition.status == 2))
-			return <JoinedAgreementActions id={id} />;
-	} else if (String(agreementStatus) == "Finalized") {
-		return (
-			<>
-				<AgreementFinalizedAlert />
-				{userPosition && <FinalizedAgreementActions id={id} userPosition={userPosition} />}
-			</>
-		);
+	if (userPosition) {
+		if (["Created", "Ongoing"].includes(String(agreementStatus))) {
+			if (userPosition.status == 0)
+				return <JoinableAgreementActions id={id} userPosition={userPosition} />;
+			if (userPosition.status == 1 || userPosition.status == 2)
+				return <JoinedAgreementActions id={id} />;
+		} else if (String(agreementStatus) == "Finalized") {
+			return <FinalizedAgreementActions id={id} userPosition={userPosition} />;
+		}
 	}
 	return <></>;
 };
--- packages/web-app/components/agreement/actions/Finalized.tsx
+++ packages/web-app/components/agreement/actions/Finalized.tsx
@@ -1,5 +1,6 @@
-import { BigNumber } from "ethers";
+// import { BigNumber } from "ethers";
 import { Button } from "@nation3/ui-components";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
 import { useAgreementWithdraw } from "../../../hooks";
 import { UserPosition } from "../context/types";
@@ -25,12 +26,31 @@ export const FinalizedAgreementActions = ({
 
 	if (userPosition.status == 2)
 		return (
-			<Button
-				label="Withdraw"
-				disabled={withdrawLoading || withdrawProcessing}
-				isLoading={withdrawLoading || withdrawProcessing}
-				onClick={() => withdraw()}
-			/>
+			<div className="grid grid-cols-2 gap-6">
+				<div className="w-full flex-col items-stretch h-full flex justify-between">
+					<div className="flex flex-col justify-between">
+						<div className="flex mb-4 items-center">
+							<span>
+								<CheckCircleIcon className="w-7 h-7 text-bluesky" />
+							</span>
+							<h3 className="text-xl text-slate-700 ml-2 font-semibold">Withdraw from agreement</h3>
+						</div>
+						<p className="text-slate-500 text-sm mb-4">
+							Withdraw you locked funds from this agreement.
+						</p>
+					</div>
+					<div className="flex w-auto">
+						<Button
+							className="flex w-auto min-w-[60%] px-10 rounded-full"
+							outlined
+							disabled={withdrawLoading || withdrawProcessing}
+							isLoading={withdrawLoading || withdrawProcessing}
+							label="Withdraw"
+							onClick={() => withdraw()}
+						/>
+					</div>
+				</div>
+			</div>
 		);
 
 	return <></>;
