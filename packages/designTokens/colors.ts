2668652fca1ec3a1b0d6ab6c7499042fa509b12f
Gordo <60603572+gordo-labs@users.noreply.github.com>
2023-03-29 23:08:05 +0200

    update: base

---
+++ packages/designTokens/colors.ts
@@ -0,0 +1,63 @@
+/* 
+
+-----------------------
+NATION 3 TAILWIND THEME
+-----------------------            
+DESIGN TOKENS
+COLORS
+LINK: https://www.figma.com/file/LJh6k4lGbEZINmWrQxJkBA/Design-Tokens?node-id=1-17 
+-----------------------
+
+*/
+
+export const colors = {
+    gradient: {
+        dark: "#1f2938",
+        light: "#f4faff",
+    },
+    neutral: {
+        100:"#FFFFFF",
+        200:"#F5FBFF",
+        300:"#E2EFF9",
+        400:"#ACC5DA",
+        500:"#7395B2",
+        600:"#426685",
+        700:"#224059",
+        800:"#172B3C",
+    },
+    primary:{
+        blue1:"#E9F8FF",
+        blue2:"#B7E5FF",
+        blue3:"#69C9FF",
+        green1:"#EAFFF5",
+        green2:"#B5FFD9",
+        green3:"#88F1BB",
+    },
+    secondary:{},
+    warning:{},
+    danger:{},
+    bluesky: {
+        100: "#D9F1FF",
+        200: "#A1DDFF",
+        300: "#69c9ff",
+        400: "#44B7F9",
+        500: "#209EE6",
+        600: "#0880C4",
+        700: "#00659F",
+        800: "#004C78",
+        900: "#003654",
+        DEFAULT: "#69c9ff",
+    },
+    greensea: {
+        100: "#D9FFE6",
+        200: "#AFF8CE",
+        300: "#88F1BB",
+        400: "#5DE2A7",
+        500: "#36D399",
+        600: "#17B689",
+        700: "#009A73",
+        800: "#00785C",
+        900: "#005441",
+        DEFAULT: "#88F1BB",
+    },
+}
---
+++ packages/designTokens/illustrations.ts
@@ -0,0 +1,11 @@
+/* 
+
+-----------------------
+NATION 3 TAILWIND THEME
+-----------------------            
+DESIGN TOKENS
+ILLUSTRATIONS
+LINK: https://www.figma.com/file/LJh6k4lGbEZINmWrQxJkBA/Design-Tokens?node-id=61-2824
+-----------------------
+
+*/
