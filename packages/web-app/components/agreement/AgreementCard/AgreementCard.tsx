f3439c404a33c75e2c1779bfbb108bbb70f5a8f9
xPi2 <uxiopineiro@gmail.com>
2023-05-19 15:28:22 +0100

    chore: Componetize Agreement card and enable terms preview on agreement preview

--- packages/web-app/components/agreement/AgreementCard/AgreementCard.tsx
+++ packages/web-app/components/agreement/AgreementCard/AgreementCard.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import {
 	Body3,
 	BodyHeadline,
@@ -7,11 +8,11 @@ import {
 	ScreenType,
 	useScreen,
 } from "@nation3/ui-components";
-import { BigNumber, utils } from "ethers";
+import { BigNumber, BigNumberish, utils } from "ethers";
 import { motion } from "framer-motion";
 import React from "react";
 import { AccountDisplay } from "../../AccountDisplay";
-import MarkdownFile from "../../MarkdownFile/MarkdownFile";
+import MarkdownFile, { MarkdownFileProps } from "../../MarkdownFile/MarkdownFile";
 import TokenRenderer from "../../TokenRenderer";
 import { InputPositionList } from "../../agreementCreate/context/types";
 import AgreementStatus from "../AgreementStatus";
@@ -31,8 +32,73 @@ type IAgreementCardProps = {
 
 const IAgreementCardDefaultProps = {};
 
+const TermsSection: React.FC<MarkdownFileProps> = (props) => {
+	const { fileStatus } = props;
+
+	return (
+		<div className="flex flex-wrap gap-min3">
+			<MarkdownFile {...props} fileStatus={fileStatus ? fileStatus : "private"} />
+			<AgreementStatus fileStatus={fileStatus} />
+		</div>
+	);
+};
+
+const PartySectionItem: React.FC<{ account: string; balance: BigNumberish }> = ({
+	account,
+	balance,
+}) => {
+	return (
+		<div className="flex justify-between">
+			<AccountDisplay address={account} />
+			<Body3>{utils.formatUnits(BigNumber.from(balance))}</Body3>
+		</div>
+	);
+};
+
+interface PartiesSectionProps {
+	positions: InputPositionList;
+}
+
+const PartiesSection: React.FC<PartiesSectionProps> = ({ positions }) => {
+	return (
+		<>
+			<div className="flex justify-between">
+				<Body3 color="neutral-c-400">Parties</Body3>
+				<Body3 color="neutral-c-400">Collateral</Body3>
+			</div>
+			<div className="flex flex-col mb-min2 gap-min2">
+				{positions.map(({ account, balance }, index) => (
+					<PartySectionItem key={index} account={account} balance={balance} />
+				))}
+			</div>
+		</>
+	);
+};
+
 const AgreementCard: React.FC<IAgreementCardProps> = (props) => {
-	const { id, title, terms, status, termsHash, fileName, fileStatus, positions, token } = props;
+	const { status, title, terms, termsHash, fileName, fileStatus, positions, token } = props;
+
+	return (
+		<AgreementCardBase>
+			<TermsSection
+				fileName={fileName}
+				termsFile={terms}
+				hash={termsHash}
+				fileStatus={fileStatus}
+				isCreating={status == "Preview"}
+			/>
+			<TokenRenderer tokenSymbol={token?.symbol} />
+			<BodyHeadline color="text-neutral-c-400">{title}</BodyHeadline>
+			<PartiesSection positions={positions} />
+		</AgreementCardBase>
+	);
+};
+
+interface CardProps {
+	children: ReactNode;
+}
+
+const AgreementCardBase: React.FC<CardProps> = ({ children }) => {
 	const { screen } = useScreen();
 	// useEffect(() => {}, []);
 
@@ -49,32 +115,7 @@ const AgreementCard: React.FC<IAgreementCardProps> = (props) => {
 		>
 			<Card size="base" className="p-[0px] sm:p-min3 md:p-base flex flex-col gap-min3 w-full">
 				<IllustrationRenderer customSize={60} icon={<N3AgreementDone />} size="sm" />
-				<div className="flex flex-wrap gap-min3">
-					<MarkdownFile
-						fileName={fileName}
-						termsFile={terms}
-						hash={fileName}
-						fileStatus={fileStatus ? fileStatus : "private"}
-					/>
-					<AgreementStatus fileStatus={fileStatus} />
-				</div>
-				<TokenRenderer tokenSymbol={token?.symbol} />
-				<BodyHeadline color="text-neutral-c-400">{title}</BodyHeadline>
-				<div className="flex justify-between">
-					<Body3 color="neutral-c-400">Parties</Body3>
-					<Body3 color="neutral-c-400">Collateral</Body3>
-				</div>
-				<div className="flex flex-col mb-min2 gap-min2">
-					{positions.map(({ account, balance }, index) => (
-						<div className="flex justify-between" key={index}>
-							<AccountDisplay key={index} address={account} />
-							<Body3>
-								{utils.formatUnits(BigNumber.from(balance))}
-								{/* ${token?.symbol ?? ""} */}
-							</Body3>
-						</div>
-					))}
-				</div>
+				{children}
 			</Card>
 		</motion.div>
 	);
