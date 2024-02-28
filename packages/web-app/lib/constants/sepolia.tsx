35a920d3ad8656206b46ff8ad34affe600430bca
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-28 13:34:32 +0700

    fix: framework contract

--- packages/web-app/lib/constants/sepolia.tsx
+++ packages/web-app/lib/constants/sepolia.tsx
@@ -2,7 +2,7 @@ import { BigNumber } from "ethers";
 
 export const permit2Address = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
 export const NATION = "0x23Ca3002706b71a440860E3cf8ff64679A00C9d7";
-export const frameworkAddress = "0x1dBEFF62DFa113f7254AaB5772F1AC6E66F94e7e";
+export const frameworkAddress = "0xD96aA6e2568f4e9632D2A5234Bb8410ca7609a27";
 export const arbitratorAddress = "0xBe67cEdCD1FE38aac8a5781A51250FDeFB344E6C";
 export const cohortAddress = "0xfbb66bc799308435ed2a0e0c0ac3ad1d46749b7b";
 export const safeTxServiceUrl = "https://safe-transaction-sepolia.safe.global";
