827e7232a9005215f18c2e0522e175112efd964b
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-29 19:27:47 +0700

    fix: error during preparing agreement

--- packages/web-app/components/dispute/ResolutionForm.tsx
+++ packages/web-app/components/dispute/ResolutionForm.tsx
@@ -14,83 +14,84 @@ import { Card } from "@nation3/ui-components";
 import { BodyHeadline } from "@nation3/ui-components";
 
 export const ResolutionForm = () => {
-	const { dispute } = useDispute();
-	const { propose } = useCohort();
-	const [settlement, setSettlement] = useState<Position[]>(dispute.positions ?? []);
-	const { frameworkAddress } = useConstants();
+  const { dispute } = useDispute();
+  const { propose } = useCohort();
+  const [settlement, setSettlement] = useState<Position[]>(dispute.positions ?? []);
+  const { frameworkAddress } = useConstants();
 
-	const settlementBalance = useMemo(() => {
-		return settlement?.reduce((result, { balance }) => result.add(balance), BigNumber.from(0));
-	}, [settlement]);
+  const settlementBalance = useMemo(() => {
+    return settlement?.reduce((result, { balance }) => result.add(balance), BigNumber.from(0));
+  }, [settlement]);
 
-	const isValidSettlement = useMemo(() => {
-		if (dispute.balance && settlementBalance) {
-			return settlementBalance.eq(dispute.balance);
-		}
-		return false;
-	}, [dispute.balance, settlementBalance]);
+  const isValidSettlement = useMemo(() => {
+    if (dispute.balance && settlementBalance) {
+      return settlementBalance.eq(dispute.balance);
+    }
+    return false;
+  }, [dispute.balance, settlementBalance]);
 
-	const submit = async () => {
-		const metadata = generateResolutionMetadata(settlement ?? []);
+  const submit = async () => {
+    const metadata = generateResolutionMetadata(settlement ?? []);
 
-		const { put, cid } = await preparePutToIPFS(metadata);
-		const metadataURI = `ipfs://${cid}`;
+    const { put } = await preparePutToIPFS(metadata);
+    const cid = await put();
+    const metadataURI = `ipfs://${cid}`;
 
-		const data = arbitratorInterface.encodeFunctionData("submitResolution", [
-			frameworkAddress,
-			dispute.id,
-			metadataURI,
-			settlement || [],
-		]);
+    const data = arbitratorInterface.encodeFunctionData("submitResolution", [
+      frameworkAddress,
+      dispute.id,
+      metadataURI,
+      settlement || [],
+    ]);
 
-		propose(data)
-			.then(async () => {
-				await put();
-				window.location.reload();
-			})
-			.catch((error) => console.log(error));
-	};
+    propose(data)
+      .then(async () => {
+        await put();
+        window.location.reload();
+      })
+      .catch((error) => console.log(error));
+  };
 
-	const updateBalance = (index: number, balance: string) => {
-		if (!settlement) return;
-		const newSettlement = settlement.map((position) => ({ ...position }));
-		newSettlement[index].balance = balance
-			? utils.parseUnits(balance)
-			: newSettlement[index].balance;
-		setSettlement(newSettlement);
-	};
+  const updateBalance = (index: number, balance: string) => {
+    if (!settlement) return;
+    const newSettlement = settlement.map((position) => ({ ...position }));
+    newSettlement[index].balance = balance
+      ? utils.parseUnits(balance)
+      : newSettlement[index].balance;
+    setSettlement(newSettlement);
+  };
 
-	return (
-		<Card className="">
-			<div>
-				<BodyHeadline className="mb-base">Settlement proposal</BodyHeadline>
-				<div className="border-2 border-neutra-c-200 rounded-full my-min3"></div>
-			</div>
-			<Table
-				columns={["participant", "stake"]}
-				data={
-					settlement?.map(({ party, balance }, index) => [
-						<AccountDisplay key={index} address={party} />,
-						<TokenBalanceInput
-							key={index}
-							defaultValue={utils.formatUnits(balance)}
-							token={dispute.collateralToken?.symbol ?? ""}
-							onChange={(e: ChangeEvent<HTMLInputElement>) => {
-								const purged = purgeFloat(e.target.value);
-								if (parseFloat(purged) < 0) return;
-								e.target.value = purged;
-							}}
-							onBlur={(e: FocusEvent<HTMLInputElement>) => {
-								if (!e.target.value) {
-									e.target.value = e.target.placeholder;
-								}
-								updateBalance(index, purgeFloat(e.target.value, { strip: true }));
-							}}
-						/>,
-					]) || []
-				}
-			/>
-			<Button label="Propose" disabled={!isValidSettlement} onClick={() => submit()} />
-		</Card>
-	);
+  return (
+    <Card className="">
+      <div>
+        <BodyHeadline className="mb-base">Settlement proposal</BodyHeadline>
+        <div className="border-2 border-neutra-c-200 rounded-full my-min3"></div>
+      </div>
+      <Table
+        columns={["participant", "stake"]}
+        data={
+          settlement?.map(({ party, balance }, index) => [
+            <AccountDisplay key={index} address={party} />,
+            <TokenBalanceInput
+              key={index}
+              defaultValue={utils.formatUnits(balance)}
+              token={dispute.collateralToken?.symbol ?? ""}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                const purged = purgeFloat(e.target.value);
+                if (parseFloat(purged) < 0) return;
+                e.target.value = purged;
+              }}
+              onBlur={(e: FocusEvent<HTMLInputElement>) => {
+                if (!e.target.value) {
+                  e.target.value = e.target.placeholder;
+                }
+                updateBalance(index, purgeFloat(e.target.value, { strip: true }));
+              }}
+            />,
+          ]) || []
+        }
+      />
+      <Button label="Propose" disabled={!isValidSettlement} onClick={() => submit()} />
+    </Card>
+  );
 };
--- packages/web-app/lib/ipfs.tsx
+++ packages/web-app/lib/ipfs.tsx
@@ -1,43 +1,26 @@
-import { Web3Storage } from "web3.storage";
-import { pack } from "ipfs-car/pack";
-import { CarReader } from "@ipld/car";
-
+import { NFTStorage, Blob } from 'nft.storage'
 const ipfsGateway = "https://w3s.link/ipfs";
-
 export const IPFSUriToUrl = (uri: string) => {
-	return `${ipfsGateway}/${uri.split("ipfs://").pop()}`;
+  return `${ipfsGateway}/${uri.split("ipfs://").pop()}`;
 };
-
-export const client = () => {
-	const token = process.env.IPFS_API_TOKEN || process.env.NEXT_PUBLIC_IPFS_API_TOKEN;
-	if (!token) {
-		throw new Error("IPFS API token not found");
-	}
-	return new Web3Storage({ token });
-};
-
+const client = new NFTStorage({
+  token: process.env.NEXT_PUBLIC_NFTSTORAGE_KEY || '',
+})
+console.log("TRIET")
+console.log(process.env)
 const objectToFile = (obj: object, filename: string) => {
-	const blob = new Blob([JSON.stringify(obj)], { type: "application/json" });
-
-	return new File([blob], filename);
+  const blob = new Blob([JSON.stringify(obj)], { type: "application/json" });
+  return new File([blob], filename);
 };
-
 const stringToFile = (str: string, filename: string) => {
-	return new File([Buffer.from(str)], filename);
+  return new File([Buffer.from(str)], filename);
 };
-
 export const preparePutToIPFS = async (data: string | object, filename?: string) => {
-	const file =
-		typeof data === "string"
-			? stringToFile(data, filename || "file.md")
-			: objectToFile(data, "data.json");
-	const { root, out } = await pack({ input: [file], wrapWithDirectory: false });
-	const cid = root.toString();
-
-	const put = async () => {
-		const car = await CarReader.fromIterable(out);
-		return client().putCar(car);
-	};
-
-	return { cid, put };
+  const d = new Blob([
+    JSON.stringify(data),
+  ])
+  const put = async () => {
+    return await client.storeBlob(d)
+  };
+  return { put };
 };
