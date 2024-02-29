926fef7e547c12037292dce0026b65fed1e05c44
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-29 19:29:26 +0700

    lint

--- packages/web-app/lib/ipfs.tsx
+++ packages/web-app/lib/ipfs.tsx
@@ -1,16 +1,16 @@
 import { NFTStorage, Blob } from "nft.storage";
 const ipfsGateway = "https://w3s.link/ipfs";
 export const IPFSUriToUrl = (uri: string) => {
-  return `${ipfsGateway}/${uri.split("ipfs://").pop()}`;
+	return `${ipfsGateway}/${uri.split("ipfs://").pop()}`;
 };
 const client = new NFTStorage({
-  token: process.env.NEXT_PUBLIC_NFTSTORAGE_KEY || "",
+	token: process.env.NEXT_PUBLIC_NFTSTORAGE_KEY || "",
 });
 
 export const preparePutToIPFS = async (data: string | object, filename?: string) => {
-  const d = new Blob([JSON.stringify(data)]);
-  const put = async () => {
-    return await client.storeBlob(d);
-  };
-  return { put };
+	const d = new Blob([JSON.stringify(data)]);
+	const put = async () => {
+		return await client.storeBlob(d);
+	};
+	return { put };
 };
