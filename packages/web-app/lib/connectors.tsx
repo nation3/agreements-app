b562e693dd674b9d7ba4700fbb42174c7e1717b5
TTNguyenDev <nttriet9a4@gmail.com>
2024-03-01 20:11:43 +0700

    feat: support sepolia

--- packages/web-app/lib/connectors.tsx
+++ packages/web-app/lib/connectors.tsx
@@ -1,63 +1,69 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { providers } from "ethers";
 import { Chain, configureChains } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { mainnet, sepolia } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
+
 
 type FallbackProviderConfig = Omit<providers.FallbackProviderConfig, "provider">;
 
 const customAlchemyProvider = ({ priority, stallTimeout, weight }: FallbackProviderConfig) => {
-	return (chain: Chain) => {
-		const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY; //mainnet
-		if (!apiKey || !chain.rpcUrls.alchemy) return null;
 
-		return {
-			chain: {
-				...chain,
-				rpcUrls: {
-					...chain.rpcUrls,
-					default: `${chain.rpcUrls.alchemy}/${apiKey}`,
-				},
-			} as Chain,
-			provider: () => {
-				const provider = new providers.AlchemyProvider(
-					{
-						chainId: chain.id,
-						name: chain.network,
-						ensAddress: chain.ens?.address,
-					},
-					apiKey,
-				);
-				return Object.assign(provider, { priority, stallTimeout, weight });
-			},
-			webSocketProvider: () =>
-				new providers.AlchemyWebSocketProvider(
-					{
-						chainId: chain.id,
-						name: chain.network,
-						ensAddress: chain.ens?.address,
-					},
-					apiKey,
-				),
-		};
-	};
+  return (chain: Chain) => {
+    let apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
+    if (chain?.id == 11155111) {
+      apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_SEPOLIA;
+
+    }
+    if (!apiKey || !chain.rpcUrls.alchemy) return null;
+
+    return {
+      chain: {
+        ...chain,
+        rpcUrls: {
+          ...chain.rpcUrls,
+          default: `${chain.rpcUrls.alchemy}/${apiKey}`,
+        },
+      } as Chain,
+      provider: () => {
+        const provider = new providers.AlchemyProvider(
+          {
+            chainId: chain.id,
+            name: chain.network,
+            ensAddress: chain.ens?.address,
+          },
+          apiKey,
+        );
+        return Object.assign(provider, { priority, stallTimeout, weight });
+      },
+      webSocketProvider: () =>
+        new providers.AlchemyWebSocketProvider(
+          {
+            chainId: chain.id,
+            name: chain.network,
+            ensAddress: chain.ens?.address,
+          },
+          apiKey,
+        ),
+    };
+  };
 };
 
 export const providersToUse = () => {
-	const providers = [customAlchemyProvider({ priority: 0 }), publicProvider({ priority: 1 })];
-	return providers;
+  const providers = [customAlchemyProvider({ priority: 0 }), publicProvider({ priority: 1 })];
+  return providers;
 };
 
 export const chainsToUse = () => {
-	return [mainnet];
+  return [mainnet, sepolia];
 };
 
 export const { chains, provider, webSocketProvider } = configureChains(
-	chainsToUse(),
-	providersToUse(),
+  chainsToUse(),
+  providersToUse(),
 );
 
 export const { connectors } = getDefaultWallets({
-	appName: "Nation3 Court App",
-	chains,
+  appName: "Nation3 Court App",
+  chains,
 });
--- packages/web-app/lib/constants/index.tsx
+++ packages/web-app/lib/constants/index.tsx
@@ -1,26 +1,29 @@
 import { BigNumber } from "ethers";
 import * as mainnet from "./mainnet";
+import * as sepolia from "./sepolia";
 
-export { mainnet };
+export { mainnet, sepolia };
 
 export interface Constants {
-	permit2Address: `0x${string}`;
-	NATION: `0x${string}`;
-	frameworkAddress: `0x${string}`;
-	arbitratorAddress: `0x${string}`;
-	cohortAddress: `0x${string}`;
-	safeTxServiceUrl: string;
-	subgraphURI: string | undefined;
-	appealCost: BigNumber | undefined;
+  permit2Address: `0x${string}`;
+  NATION: `0x${string}`;
+  frameworkAddress: `0x${string}`;
+  arbitratorAddress: `0x${string}`;
+  cohortAddress: `0x${string}`;
+  safeTxServiceUrl: string;
+  subgraphURI: string | undefined;
+  appealCost: BigNumber | undefined;
 }
 
 const constants = (chainId: number) => {
-	switch (chainId) {
-		case 1:
-			return { ...mainnet };
-		default:
-			return { ...mainnet };
-	}
+  switch (chainId) {
+    case 1:
+      return { ...mainnet };
+    case 11155111:
+      return { ...sepolia };
+    default:
+      return { ...mainnet };
+  }
 };
 
 export default constants;
